c7fabb53462015aa15b940816f9c4b397c61fa24
Alan Nagashima <alanitro.code@outlook.com>
2026-02-03 03:13:29 +1100

    add imgui

--- source/Nitrocraft.cpp
+++ source/Nitrocraft.cpp
@@ -3,6 +3,9 @@
 #include <print>
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include "World.hpp"
 #include "Graphics_WorldRenderer.hpp"
 #include "Graphics_Camera.hpp"
@@ -12,6 +15,7 @@ namespace
 {
 enum State
 {
+    INACTIVE,
     ACTIVE,
     PAUSE,
 };
@@ -137,12 +141,58 @@ void RecalculateCamera(Camera& camera, GLFWwindow* window, double delta_time)
     //// Recalculate camera
     camera.Calculate(delta_position, delta_rotation);
 }
+
+void ImGUI_Initialize(GLFWwindow* window)
+{
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+    ImGui_ImplOpenGL3_Init();
+
+    ImGui::StyleColorsDark();
+}
+
+void ImGUI_Terminate()
+{
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+}
+
+void ImGUI_NewFrame()
+{
+    // (Your code calls glfwPollEvents())
+    // ...
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+}
+
+void ImGUI_Render()
+{
+    // Rendering
+    // (Your code clears your framebuffer, renders your other stuff etc.)
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    // (Your code calls glfwSwapBuffers() etc.)
+}
+
 } // namespace unnamed
 
 void Nitrocraft::Run()
 {
     // Initialization
     GLFWwindow* window = InitializeGLFWAndOpenGLContext();
+
+    ImGUI_Initialize(window);
 
     bool is_running = true;
 
@@ -170,6 +220,7 @@ void Nitrocraft::Run()
     while (is_running)
     {
         //// Update
+        ImGUI_NewFrame();
 
         /*if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         {
@@ -207,10 +258,54 @@ void Nitrocraft::Run()
 
         world_renderer.PrepareChunksToRender(world.GetActiveArea());
 
+        if (ImGui::Begin("Information & Settings"))
+        {
+            int width, height;
+            glfwGetFramebufferSize(window, &width, &height);
+
+            auto io = ImGui::GetIO();
+            ImGui::Text("Frame Rate : %d FPS", (int)io.Framerate);
+            ImGui::Text("Frame Time : %.3f ms/frame", 1000.0f / io.Framerate);
+            ImGui::Text("Resolution : %d x %d", width, height);
+            ImGui::Text(" ");
+
+            constexpr const char* state_names[3]{ "Inactive", "Active", "Pause" };
+            ImGui::Text("State : %s", state_names[(int)state]);
+            ImGui::Text(" ");
+
+            auto id = FromWorldPositionToChunkID(camera.GetPosition());
+            ImGui::Text("Chunk ID : %d %d", id.x, id.z);
+            ImGui::Text(" ");
+
+            ImGui::Text("X Position : %.2f", camera.GetPosition().x);
+            ImGui::Text("Y Position : %.2f", camera.GetPosition().y);
+            ImGui::Text("Z Position : %.2f", camera.GetPosition().z);
+            ImGui::Text(" ");
+
+            ImGui::Text("X Rotation : %.2f", camera.GetFront().x);
+            ImGui::Text("Y Rotation : %.2f", camera.GetFront().y);
+            ImGui::Text("Z Rotation : %.2f", camera.GetFront().z);
+            ImGui::Text(" ");
+
+            static bool line_mode = false;
+            ImGui::Checkbox(" Line Mode", &line_mode);
+            if (line_mode)
+            {
+                glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            }
+            else
+            {
+                glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            }
+        }
+        ImGui::End();
+
         //// Render
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         world_renderer.Render(camera);
+
+        ImGUI_Render();
 
         glfwSwapBuffers(window);
 
@@ -221,6 +316,8 @@ void Nitrocraft::Run()
     world.Terminate();
 
     world_renderer.Terminate();
+
+    ImGUI_Terminate();
     
     glfwDestroyWindow(window);
 
